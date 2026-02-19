5718b9770e06f575655832ec4418b8a1983d82c7
atagaev95 <predator-man@mail.ru>
2026-02-19 22:04:20 +0300

    refactor logging and utility functions

--- app/tools/utils.py
+++ app/tools/utils.py
@@ -1,6 +1,7 @@
 import re
 from datetime import datetime
 
+
 def clean_content(content: str) -> str:
     """Очищает строку от нежелательных символов."""
     return content.strip()
@@ -9,7 +10,8 @@ def is_valid_url(url: str) -> bool:
     """Проверяет, является ли строка валидным URL."""
     regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
+        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
         r'localhost|' # localhost...
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
         r'(?::\d+)?' # optional port

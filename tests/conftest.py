0b6c7530c2bea52c8a7c9ffa46a15e3e1bbfe155
atagaev95 <predator-man@mail.ru>
2026-02-19 22:04:20 +0300

    update test bot configuration and prefix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
-import pytest
-import pytest_asyncio
 import discord
+import pytest_asyncio
 from discord.ext import commands
+
 from app.cogs.general import General
 
+
 @pytest_asyncio.fixture
-async def bot():
+async def bot() -> commands.Bot:
+    """Фикстура для создания экземпляра бота с загруженным когом General."""
     intents = discord.Intents.default()
     intents.message_content = True
-    bot = commands.Bot(command_prefix="!", intents=intents)
+    bot = commands.Bot(command_prefix="?", intents=intents, help_command=None)
     await bot.add_cog(General(bot))
     return bot
